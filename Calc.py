4245a16b4ec299bd77e6e5ac316c21a40f3f8042
amywandd <njeriamy2@gmail.com>
2023-09-29 06:35:17 -0700

    A simple calculator to compute sum

---
+++ Calc.py
@@ -0,0 +1,7 @@
+# Amy Njeri Wanderi - sct211-0010/2021
+name = input("What is your name?")
+print("Welcome ", name)
+num1 = int(input("Enter the first number"))
+num2 = int(input("Enter the second number"))
+total = num1 + num2
+print("The addition of ", num1, "and ", num2, "is ", total)
